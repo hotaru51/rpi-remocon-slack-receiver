8d05af870e0032e23a8f71c9e624a3e4d7896879
hotaru <36354847+hotaru51@users.noreply.github.com>
2020-07-26 00:31:37 +0900

    リソース作成

--- lib/rpi-remocon-slack-receiver-stack.ts
+++ lib/rpi-remocon-slack-receiver-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigateway from '@aws-cdk/aws-apigateway'
+import * as sqs from '@aws-cdk/aws-sqs'
+import * as iam from '@aws-cdk/aws-iam'
 
 export class RpiRemoconSlackReceiverStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    // The code that defines your stack goes here
-  }
+        // SQS
+        const receiverQueueProps: sqs.QueueProps = {
+            queueName: 'RpiRemoconSlackReceiverQueue'
+        }
+        const receiverQueue = new sqs.Queue(this, 'receiverQueue', receiverQueueProps)
+
+        // Lambda
+        const receiverLambdaProps: lambda.FunctionProps = {
+            functionName: 'RpiRemoconSlackReceiverFunction',
+            runtime: lambda.Runtime.RUBY_2_7,
+            code: lambda.AssetCode.fromAsset('lambda/event-receiver'),
+            handler: 'app.handler',
+            timeout: cdk.Duration.seconds(3),
+            environment: {
+                SQS_URL: receiverQueue.queueUrl
+            }
+        }
+        const receiverLambda = new lambda.Function(this, 'receiverLambda', receiverLambdaProps)
+        receiverLambda.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['sqs:SendMessage'],
+            resources: [receiverQueue.queueArn]
+        }))
+
+        // API Gateway
+        const receiverApiGatewayProps: apigateway.LambdaRestApiProps = {
+            restApiName: 'RpiRemoconSlackReceiverApi',
+            handler: receiverLambda
+        }
+        const receiverApiGateway = new apigateway.LambdaRestApi(this, 'receiverApiGateway', receiverApiGatewayProps)
+    }
 }
