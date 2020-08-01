1430631e2cd47f2f3dca6bc6cc01419955bdec17
hotaru <36354847+hotaru51@users.noreply.github.com>
2020-08-01 22:39:15 +0900

    SQSへのイベント送信実装

--- lambda/event-receiver/app.rb
+++ lambda/event-receiver/app.rb
@@ -1,16 +1,40 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'aws-sdk'
 
 def handler(event:, context:)
-  body_json = JSON.generate({ message: 'nothing' })
-  body_json = JSON.generate(event['body']) unless event['body'].nil?
+  puts 'received event'
+  puts JSON.generate(event)
 
+  return response 200, { message: 'nothing' } if event.dig('body').nil?
+
+  begin
+    body_hash = JSON.parse event['body']
+    puts 'Slack event body'
+    puts JSON.generate body_hash
+  rescue RuntimeError => e
+    puts e
+    return response 501, { message: 'could not parse the event body' }
+  end
+
+  return response 200, **{ challenge: body_hash['challenge'] } if body_hash['type'] == 'url_verification'
+
+  queue = Aws::SQS::Client.new(region: 'ap-northeast-1')
+  result = queue.send_message(
+    queue_url: ENV['SQS_URL'], message_body: JSON.generate(body_hash)
+  )
+  puts 'send message result'
+  puts JSON.generate(result)
+  response 200, { message: 'ok' }
+end
+
+def response(status_code, body)
   {
-    statusCode: 200,
+    statusCode: status_code,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: body_json
+    body: JSON.generate(body)
   }
 end
